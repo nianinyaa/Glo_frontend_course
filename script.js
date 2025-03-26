042dd87a51713042a696547874b477a685b22400
Криницын Динар Олегович <dokrinits1@www.mts.ru>
2025-03-26 23:06:43 +0300

    добавлен console.log()

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-const div = document.getElementById('github')
+const div = document.getElementById("github");
 
-const logged = ( ){
-    console.log(div.textContent);
-}
+const logged = () => {
+  console.log(div.textContent);
+  console.log("commits");
+};
