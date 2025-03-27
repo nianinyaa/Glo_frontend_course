de122705c901c5f3769ab6bddcc9f5d04c826cc9
Криницын Динар Олегович <dokrinits1@www.mts.ru>
2025-03-27 21:20:53 +0300

    создан lesson01

--- script.js
+++ script.js
@@ -6,4 +6,14 @@ const logged = () => {
   console.log("Наконец-то мы узнали все команды, необходимые для работы с GIT");
 };
 
-console.log("Hello world");
+const title = 10;
+const screens = 9;
+const screenPrice = 8;
+const rollback = 7;
+const fullPrice = 6;
+const adaptive = 5;
+
+let greet = prompt("What is your name, brooo?");
+alert(`Good morning ${greet}!`);
+
+console.log("Homie, you are the best");
