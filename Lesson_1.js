628d1209d5042c3a5166e2672db2474ab839393c
Криницын Динар Олегович <dokrinits1@www.mts.ru>
2025-04-05 01:02:47 +0300

    Лекция 3 домашнее задание

---
+++ Lesson_1.js
@@ -0,0 +1,21 @@
+
+// const a = "Переменная из lesson03";
+
+// const b = "Наша ветка  ";
+
+// const logged = () => {
+//   console.log("Наконец-то мы узнали все команды, необходимые для работы с GIT");
+// };
+
+// // let title = 10;
+// // let screens = 9;
+// // let screenPrice = 8;
+// // let rollback = 7;
+// // let fullPrice = 6;
+// // let adaptive = 5;
+
+// let greet = prompt("What is your name, brooo?");
+// alert(`Good morning ${greet}!`);
+
+// console.log("Homie, you are the best");
+// 123;
--- script.js
+++ script.js
@@ -1,20 +1,71 @@
-const a = "Переменная из lesson03";
+"use strict";
+//Урок 3.//
 
-const b = "Наша ветка  ";
+// Задание 3.
+let titleee = prompt("Как называется ваш проект?");
+console.log(titleee);
 
-const logged = () => {
-  console.log("Наконец-то мы узнали все команды, необходимые для работы с GIT");
-};
+// Задание 4.
+let screens = prompt(
+  "Какие типы экранов нужно разработать?",
+  "Простые, Сложные, Интерактивные"
+);
+console.log(screens);
 
-const title = 10;
-const screens = 9;
-const screenPrice = 8;
-const rollback = 7;
-const fullPrice = 6;
-const adaptive = 5;
+// Задание 5.
+let screenPrice = prompt("Сколько будет стоить данная работа?", "12000");
+console.log(screenPrice);
 
-let greet = prompt("What is your name, brooo?");
-alert(`Good morning ${greet}!`);
+// Задание 6.
+let adaptive = confirm("Нужен ли адаптив на сайте?");
 
-console.log("Homie, you are the best");
-123;
+if (adaptive) {
+  console.log(true);
+} else {
+  console.log(false);
+}
+
+//Задание 7. //
+let service1 = prompt("Какой дополнительный тип услуги нужен?");
+console.log(service1);
+
+let servicePrice1 = prompt("Сколько это будет стоить?");
+console.log(servicePrice1);
+
+let service2 = prompt("Какой дополнительный тип услуги нужен");
+console.log(service2);
+
+let servicePrice2 = prompt("Сколько это будет стоить?");
+console.log(servicePrice2);
+
+//Задание 8.//
+let screenPrice8 = Number(prompt("Стоимость верстки экранов", "100"));
+let servicePrice18 = Number(prompt("Стоимость дополнительной услуги 1", "100"));
+let servicePrice28 = Number(prompt("Стоимость дополнительной услуги 2", "100"));
+let fullPrice = screenPrice + servicePrice1 + servicePrice2;
+console.log(fullPrice);
+
+//Задание 9.//
+
+let screenPrice1 = Number(prompt("Цена верстки экрана", "100"));
+let servicePrice2 = Number(prompt("Цена доп. услуги №1", "100"));
+let servicePrice3 = Number(prompt("Цена доп. услуги №2", "100"));
+let finalPrice = screenPrice1 + servicePrice2 + servicePrice3;
+let expenses = Number(prompt("Откат", "150"));
+let servicePercentPrice1 = Math.ceil (finalPrice - expenses);
+console.log(servicePercentPrice1); 
+
+
+//Задание 10. Учесть варианты 0, 15000 и 30000(к какому уровню не важно) - была не очень понятна формулировка с уровнями //
+
+let fullPrice;
+
+if (fullPrice > 30000) {
+  console.log("Даем скидку в 10%");
+} else if (fullPrice > 15000 && fullPrice <= 30000) {
+  console.log("Даем скидку в 5%");
+} else if (fullPrice > 0 && fullPrice <= 15000) {
+  console.log("Скидка не предусмотрена");
+} else if (fullPrice < 0) {
+  console.log("Что то пошло не так");
+}
