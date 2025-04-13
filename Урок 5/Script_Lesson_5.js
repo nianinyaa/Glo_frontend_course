4017beffbf37c3e2d63dca081f1685d3d6b1025c
Криницын Динар Олегович <dokrinits1@www.mts.ru>
2025-04-13 18:14:32 +0300

    Ещё изменения

--- Урок 5/Script_Lesson_5.js
+++ Урок 5/Script_Lesson_5.js
@@ -24,35 +24,9 @@ const getAllServicePrices = function(screenPrice){
       return `Значение не является числом. Введите число.`
   }
   if (screenPrice<=0){
-      return Число должнно быть положительным. Введите заново.
+      return "Число должнно быть положительным. Введите заново".
   }
   console.log(`Значение является числом.`) 
 } 
 
 
-
-
-//Усложненное 1
-
-// do {
-//   screenPrice = prompt("Сколько будет стоить данная работа?", "1200");
-
-//   if (sсreenPrice === null) {
-//     sсreenPrice = null; //почемк здесь выводим скринпрайс, а не ретерн
-//     break;
-//   }
-//   screenPrice = screenPrice.trim().replace(/\s+/g, "");
-//   screenPrice = screenPrice === "" ? NaN : parseFloat(screenPrice);
-
-//   if (isNaN(screenPrice)) {
-//     alert("Значение не является числом. Пожалуйста, введите число");
-//   }
-// } while (isNaN(screenPrice));
-
-// //Проверка - логгинг
-
-// if (screenPrice === null) {
-//   console.log("Ввод отменили");
-// } else {
-//   console.log("Полученное число", screenPrice, typeof screenPrice);
-// }
